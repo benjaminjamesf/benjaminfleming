beecf90689d3ce44fbb8eeca7baa5bd6c332ed50
benjie <benjaminjamesf@gmail.com>
2019-02-14 19:08:54 +1100

    Portfolio V1
    Linked pages etc

--- js/customScript.js
+++ js/customScript.js
@@ -1,29 +1,51 @@
-  $(document).ready(function() {
-        $('.showprocess').click(function() {
-                $('.toggle').slideToggle("slow");
-        });
+$(document).ready(function () {
+var $gallery = $('.gallery').flickity({
+    // options
+    imagesLoaded: true,
+    cellAlign: 'left',
+    contain: true
+});
 
-        $("#fold").click(function () {
-        $(".fold_p").fadeOut(function () {
-            $(".fold_p").text(($(".fold_p").text() == '+ Show design process') ? '- Hide design process' : '+ Show design process').fadeIn();
-            })
-         })
+var $gallery2 = $('.gallery2').flickity({
+    // options
+    imagesLoaded: true,
+    cellAlign: 'left',
+    contain: true
+});
+
+
+
+
+
+    $('.showprocess').click(function () {
+        $('.toggle').slideToggle("slow");
+        $gallery.flickity('resize');
+        $gallery2.flickity('resize');
     });
 
+    $("#fold").click(function () {
+        $(".fold_p").fadeOut(function () {
+            $(".fold_p").text(($(".fold_p").text() == '+ Show design process') ? '- Hide design process' : '+ Show design process').fadeIn();
+        })
+        
 
-$('.gallery').flickity({
-  // options
-    imagesLoaded: true,
-  cellAlign: 'left',
-  contain: true
-});
-
-$('.gallery2').flickity({
-  // options
-    imagesLoaded: true,
-  cellAlign: 'left',
-  contain: true
+    })
+    
 });
 
 
+window.onscroll = function () {
+    scrollFunction()
+};
 
+function scrollFunction() {
+    if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 420) {
+        document.getElementById("selfportrait").style.width = "80px";
+        document.getElementById("squareanimate").style.width = "80px";
+        document.getElementById("squareanimate").style.height = "80px";
+    } else {
+        document.getElementById("selfportrait").style.width = "120px";
+        document.getElementById("squareanimate").style.width = "120px";
+        document.getElementById("squareanimate").style.height = "120px";
+    }
+}
